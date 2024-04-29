85b47f003fc06193dd5e68c859cd4dcaea5f3748
mxnv <vadim.beller@mail.ru>
2024-04-29 20:56:05 +0300

    #2 commit

---
+++ newmodule/src/Main.java
@@ -0,0 +1,21 @@
+public class Main {
+    public static void main(String[] args) {
+        //Xiaomi
+        Xiaomi xiaomi = new Xiaomi();
+        xiaomi.powerOn();
+        xiaomi.setAllCharacteristics("Xiaomi Note 3 Pro", "3000 mAh", "Yellow", "3000 MHz", "1980x1080");
+        xiaomi.getAllCharacteristics();
+        xiaomi.powerOff();
+        //Iphone
+        Iphone iphone = new Iphone();
+        iphone.powerOn();
+        iphone.setAllCharacteristics("Apple Iphone 12", "2900 mAh", "White", "3150 MHz", "1980x1080");
+        iphone.getAllCharacteristics();
+        iphone.powerOff();
+    }
+
+
+
+
+
+}
